41aa1af350d7e336a40fb903a25ac994ce946815
deftonjke <defton@mail.ru>
2021-08-31 02:18:21 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let stack = [];
+  let split = str.split('');
+  split.forEach(el => {
+  	if (el === bracketsConfig[0]) {
+    	stack.push(el);
+    } else if (el === bracketsConfig[1]) {
+    	stack.pop();
+    }
+  });
+  return (stack.length === 0) ? true : false;
 }
